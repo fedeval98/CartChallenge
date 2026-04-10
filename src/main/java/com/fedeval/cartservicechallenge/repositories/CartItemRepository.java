d9ba6f85ede5574908693f168afb626b9354d76d
fedeval98 <fede.val9806@gmail.com>
2026-04-10 14:20:54 -0300

    Correccion de errores en repos

--- src/main/java/com/fedeval/cartservicechallenge/repositories/CartItemRepository.java
+++ src/main/java/com/fedeval/cartservicechallenge/repositories/CartItemRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
-    List<CartItem> findByCartId(Long cartId);
+    Optional<CartItem> findByCartCodeAndProductCode(String cartCode, String productCode);
+    List<CartItem> findByCartCode(String code);
 }
--- src/main/java/com/fedeval/cartservicechallenge/repositories/ClientRepository.java
+++ src/main/java/com/fedeval/cartservicechallenge/repositories/ClientRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findbyEmail (String email);
+    Optional<Client> findByEmail (String email);
 }
